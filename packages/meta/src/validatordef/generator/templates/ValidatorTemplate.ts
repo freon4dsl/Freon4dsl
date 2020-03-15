29cb3cc8e169b028c4e1a30ab66948d7c702b661
Jos Warmer <jos.warmer@openmodeling.nl>
2020-03-15 17:31:44 +0100

    Fix validator

--- packages/meta/src/validatordef/generator/templates/ValidatorTemplate.ts
+++ packages/meta/src/validatordef/generator/templates/ValidatorTemplate.ts
@@ -1,5 +1,5 @@
 import { Names } from "../../../utils/Names";
-import { PiLanguage, PiLangElementProperty } from "../../../languagedef/metalanguage/PiLanguage";
+import { PiLanguage } from "../../../languagedef/metalanguage/PiLanguage";
 import { PiValidatorDef } from "../../metalanguage/PiValidatorDefLang";
 
 export class ValidatorTemplate {
@@ -8,13 +8,11 @@ export class ValidatorTemplate {
 
     generateValidator(language: PiLanguage, validdef: PiValidatorDef): string {
         console.log("Creating Validator");
-        const allLangConcepts : string = Names.allConcepts(language);   
-        const langConceptType : string = Names.languageConceptType(language);     
+        const allLangConcepts : string = Names.allConcepts(language);
+        const langConceptType : string = Names.languageConceptType(language);
         const generatedClassName : String = Names.validator(language, validdef);
-        const validatorInterfaceName : String = Names.validatorInterface(language);
-        const errorClassName : String = Names.errorClassName(language);
 
-        // language.concepts.map(concept => 
+        // language.concepts.map(concept =>
         //     concept.parts.forEach(p => {
 
         //     })
@@ -29,50 +27,44 @@ export class ValidatorTemplate {
         // Template starts here
         return `
         import { ${allLangConcepts} } from "../../language";
-        import { ${validatorInterfaceName} } from "../../language/${validatorInterfaceName}";
-        import { ${errorClassName} } from "../../language/${validatorInterfaceName}";
+        import { PiValidator, PiError } from "@projectit/core";
         import { ${langConceptType} } from "../../language/${language.name}";   
         import { ${language.concepts.map(concept => `
                 ${concept.name}`).join(", ")} } from "../../language";     
         
-        export class ${generatedClassName} implements ${validatorInterfaceName} {
-
-            public validate(modelelement: ${allLangConcepts}, includeChildren?: boolean) : ${errorClassName}[]{
-                let result : ${errorClassName}[] = [];
-
+        export class ${generatedClassName} implements PiValidator {
+            public validate(modelelement: ${allLangConcepts}, includeChildren?: boolean) : PiError[]{
+                let result : PiError[] = [];
                 ${language.concepts.map(concept => `
                 if(modelelement instanceof ${concept.name}) {
                     result.concat( this.validate${concept.name}(modelelement, includeChildren) );
                 }`).join("")}
-
                 return result;
             }
-
             ${language.concepts.map(concept => `
-                private validate${concept.name}(modelelement: ${concept.name}, includeChildren?: boolean) : ${errorClassName}[]{
-                    let result : ${errorClassName}[] = [];
+                private validate${concept.name}(modelelement: ${concept.name}, includeChildren?: boolean) : PiError[]{
+                    let result : PiError[] = [];
                     // include validations here
-
                     ${((concept.parts.length > 0)?
-                    `if(!(includeChildren === undefined) && includeChildren) { 
+                `if(!(includeChildren === undefined) && includeChildren) { 
                         ${concept.parts.map( part =>
-                            (part.isList ?
-                                `modelelement.${part.name}.forEach(p => {
+                    (part.isList ?
+                            `modelelement.${part.name}.forEach(p => {
                                     result.concat( this.validate${part.type.name}(p, includeChildren) );
                                 });`
                             :
-                                `result.concat( this.validate${part.type.name}(modelelement.${part.name}, includeChildren) );`
-                            )
-                        ).join("\n")}
+                            `result.concat( this.validate${part.type.name}(modelelement.${part.name}, includeChildren) );`
+                    )
+                ).join("\n")}
                     }`
-                    : ``
-                    )}
+                : ``
+        )}
                     // check rules of baseconcept(s)
                     ${((!!concept.base )?
-                        `result.concat( this.validate${concept.base.name}(modelelement, includeChildren) );`
-                    :
-                        ``
-                    )}
+                `result.concat( this.validate${concept.base.name}(modelelement, includeChildren) );`
+                :
+                ``
+        )}
                     return result;
                 }`).join("\n")}
         }`;
