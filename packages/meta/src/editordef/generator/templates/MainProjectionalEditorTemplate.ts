243b1b7bdd65b780f7207be101876c9310881360
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-04-04 13:21:21 +0200

    Changed generation of Environment

--- packages/meta/src/editordef/generator/templates/MainProjectionalEditorTemplate.ts
+++ packages/meta/src/editordef/generator/templates/MainProjectionalEditorTemplate.ts
@@ -1,66 +1,37 @@
 import { Names } from "../../../utils/Names";
-import { PathProvider, PROJECTITCORE } from "../../../utils/PathProvider";
+import { ENVIRONMENT_GEN_FOLDER, PathProvider, PROJECTITCORE } from "../../../utils/PathProvider";
 import { PiLanguageUnit } from "../../../languagedef/metalanguage/PiLanguage";
 
 export class MainProjectionalEditorTemplate {
     constructor() {
     }
 
-    generateEditor(language: PiLanguageUnit, withToolbar: boolean): string {
+    generateMainProjectionalEditor(language: PiLanguageUnit, withToolbar: boolean, relativePath: string): string {
             // TODO use Names and PathProvider for MyToolbarComponent and its import statement
             return `
-            import { observable } from "mobx";
+            
             import { observer } from "mobx-react";
             import * as React from "react";
             
-            import { ${Names.PiEditor}, ${Names.ProjectionalEditor}, ${Names.CompositeProjection} } from "${PROJECTITCORE}";
+            import { ${Names.PiEditor}, ${Names.ProjectionalEditor} } from "${PROJECTITCORE}";
+            import { ${Names.environment(language)} } from "${relativePath}${ENVIRONMENT_GEN_FOLDER}/${Names.environment(language)}";
             
-            import { ${Names.projection(language)} } from "../${Names.projection(language)}";
-            import { ${Names.projectionDefault(language)} } from "./${Names.projectionDefault(language)}";
-            import { ${Names.actions(language)} } from "./${Names.actions(language)}";
-            import { ${Names.context(language)} } from "./${Names.context(language)}";
-            import { ${Names.editor(language)} } from "./${Names.editor(language)}";
-            import { MyToolbarComponent } from "../../webapp/toolbars/MyToolbarComponent";
-
             @observer
-            export class MainProjectionalEditor extends React.Component< any, {}> {
-                private privateEditor: ${Names.editor((language))};
+            export class MainProjectionalEditor extends React.Component<any, {}> {
             
                 constructor(props: any) {
                     super(props);
-                    this.initEditors();
-                }
-                
-                render() {
-                    var editor: ${Names.editor((language))} = this.privateEditor;
-                    return (
-                    ${ withToolbar ? `
-                        < div>
-                            {editor.mytoolbarItems && (editor.mytoolbarItems.length > 0 && (
-                                <MyToolbarComponent editor={editor} toolbar={editor}/>
-                            ))}
-                            <div>
-                                <ProjectionalEditor editor={editor}/>
-                            </div>
-                        </div>
-                    ` : `
-                        < div>
-                            <ProjectionalEditor editor={editor}/>
-                        </div>
-                    `}       
-                    );
                 }
             
-                initEditors() {
-                    const context = new ${Names.context(language)}();
-                    const actions = new ${Names.actions(language)}();
-                    const rootProjection = new CompositeProjection();
-                    const projectionManual = new ${Names.projection(language)}();
-                    const projectionDefault = new ${Names.projectionDefault(language)}();
-                    rootProjection.addProjection("manual", projectionManual);
-                    rootProjection.addProjection("default", projectionDefault);
-                    this.privateEditor = new ${Names.editor((language))}(context, rootProjection, actions);
-                    projectionDefault.setEditor(this.privateEditor);
+                render() {
+                    var editor: PiEditor = ${Names.environment(language)}.getInstance().editor;
+                    return (
+                        <div>
+                            <div>
+                                {(${Names.environment(language)}.getInstance() as ${Names.environment(language)}).projectionalEditorComponent}
+                            </div>
+                        </div>
+                    );
                 }
             }
 
