fbb08be84dc3b880c02e497351305a29c1543d75
Jos Warmer <jos.warmer@openmodeling.nl>
2020-04-04 16:03:48 +0200

    Merge development into editor specification

--- packages/playground/src/demo/unparser/DemoUnparser.ts
+++ packages/playground/src/demo/unparser/DemoUnparser.ts
@@ -1,4 +1,4 @@
-import { AllDemoConcepts, DemoAttributeType } from "./../language";
+import { AllDemoConcepts, DemoAttributeType } from "../language/gen";
 import { PiValidator, PiError, PiTyper } from "@projectit/core";
 import {
     DemoModel,
@@ -26,7 +26,7 @@ import {
     DemoFunctionCallExpression,
     DemoIfExpression,
     DemoVariableRef
-} from "./../language";
+} from "../language/gen";
 
 export class DemoUnparser {
 
