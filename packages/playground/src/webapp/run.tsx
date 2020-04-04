68e4ed85db741bbe98485dc35d818f9bcd69b237
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-04-04 14:06:20 +0200

    Moved webpack to enable use for both demo and taxrules

---
+++ packages/playground/src/webapp/run.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Provider } from "mobx-react";
+import { RouterStore } from "./app/stores";
+import { MainProjectionalEditor } from "../demo/editor";
+
+const STORE_ROUTER = "router";
+// prepare MobX stores
+const history = createBrowserHistory();
+const routerStore = new RouterStore(history);
+const rootStores = {
+    [STORE_ROUTER]: routerStore
+};
+
+// render react DOM
+// ReactDOM.render(
+//     <Provider {...rootStores}>
+//         <Root>
+//             <Router history={history}>
+//                 <Switch>
+//                     <Route path="/" component={MainProjectionalEditor} />
+//                 </Switch>
+//             </Router>
+//         </Root>
+//     </Provider>,
+//     document.getElementById("root")
+// );
+ReactDOM.render(
+    <Provider {...rootStores}>
+        <MainProjectionalEditor />
+    </Provider>,
+    document.getElementById("root")
+);
