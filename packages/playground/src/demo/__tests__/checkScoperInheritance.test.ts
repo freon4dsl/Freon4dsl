ce4f37f3fa8c36103dbcdfd02959a2c9fdfe8714
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-04-16 16:19:39 +0200

    introduced the possibility to define inheritance in the scoper definition

---
+++ packages/playground/src/demo/__tests__/checkScoperInheritance.test.ts
@@ -0,0 +1,38 @@
+import { DemoScoper } from "../scoper/gen/DemoScoper";
+import { DemoModel, DemoFunction, DemoEntity } from "../language/gen";
+import { DemoModelCreator } from "./DemoModelCreator";
+
+describe("testing Scoper", () => {
+    let modelCreator = new DemoModelCreator();
+    let inheritanceModel : DemoModel = modelCreator.createInheritanceModel();
+    let scoper = new DemoScoper();
+
+    beforeEach(done => {
+        done();
+    });
+
+    function testInheritedPropsrecursive(ent: DemoEntity, vis: string[]) {
+        if (!!ent.baseEntity) {
+            // extra props should be visible
+            ent.baseEntity.referred.attributes.forEach(attr => {
+                expect(vis).toContain(attr.name);
+            });
+            testInheritedPropsrecursive(ent.baseEntity.referred, vis);
+        }
+    }
+
+    test("inheritance", () => {
+        inheritanceModel.entities.forEach(ent => {
+            let vis = scoper.getVisibleNames(ent);
+            expect(vis).toContain(ent.name);
+            ent.attributes.forEach(attr => {
+                expect(vis).toContain(attr.name);
+            });
+            testInheritedPropsrecursive(ent, vis);
+            console.log("visible elements for " + ent.name + ":");
+            vis.forEach(n => {console.log(n);});
+
+        });
+
+    });
+});
