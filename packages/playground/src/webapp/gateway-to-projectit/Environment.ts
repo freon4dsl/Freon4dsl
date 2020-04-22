5498b0e6ea0a6e36739edf238911e1e519a51aa3
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-04-22 11:15:57 +0200

    scoper gets typer from Environment

--- packages/playground/src/webapp/gateway-to-projectit/Environment.ts
+++ packages/playground/src/webapp/gateway-to-projectit/Environment.ts
@@ -1,9 +1,11 @@
 import { PiEnvironment } from "@projectit/core";
 import { DemoEnvironment } from "../../demo/environment/gen/DemoEnvironment";
 import { TaxRulesEnvironment } from "../../taxrules/environment/gen/TaxRulesEnvironment";
+// import { CalculatorEnvironment } from "../../calculator/environment/gen/CalculatorEnvironment";
 
 /**
  * The one and only reference to the actual language for which this editor runs
  */
+// export const environment: PiEnvironment = CalculatorEnvironment.getInstance();
 export const environment: PiEnvironment = DemoEnvironment.getInstance();
 // export const environment: PiEnvironment = TaxRulesEnvironment.getInstance();
