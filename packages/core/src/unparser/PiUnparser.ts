667214d58f3feaebb39d28d26d5d99ffbd7e0a3b
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-05-17 20:31:10 +0200

    a preliminary commit: half of the content  of the documentation is written

--- packages/core/src/unparser/PiUnparser.ts
+++ packages/core/src/unparser/PiUnparser.ts
@@ -1,8 +1,9 @@
 import { PiElement } from "../language";
 
 // Part of the ProjectIt Framework.
-
+// tag::unparser-interface[]
 export interface PiUnparser {
     // returns the type of 'modelelement' according to the type rules in the Typer Definition
     unparse(modelelement: PiElement): string;
 }
+// end::unparser-interface[]
