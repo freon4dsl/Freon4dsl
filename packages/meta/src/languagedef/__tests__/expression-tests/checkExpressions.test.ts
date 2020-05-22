d24e50ff0e8f0a1563a105af051620c7cd5a8a69
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-05-22 11:55:20 +0200

    added test on language expressions: applied feature

--- packages/meta/src/languagedef/__tests__/expression-tests/checkExpressions.test.ts
+++ packages/meta/src/languagedef/__tests__/expression-tests/checkExpressions.test.ts
@@ -1,6 +1,6 @@
 import { LanguageParser } from "../../parser/LanguageParser";
 import { LanguageExpressionParser } from "../../parser/LanguageExpressionParser";
-import { PiInstance, PiLangFunctionCallExp, PiLanguageUnit, PiLimitedConcept } from "../../metalanguage";
+import { PiInstance, PiLangFunctionCallExp, PiLangSelfExp, PiLanguageUnit, PiLimitedConcept } from "../../metalanguage";
 
 describe("Checking expression on referedElement", () => {
     let testdir = "src/languagedef/__tests__/expression-tests/expressionDefFiles/";
@@ -44,13 +44,16 @@ describe("Checking expression on referedElement", () => {
             let BBconceptExps = readTest.conceptExps.find(ce => ce.conceptRef.name === "BB");
             // set of expressions should refer to some concept or interface in the language
             expect(BBconceptExps).not.toBeNull();
+            expect(BBconceptExps).not.toBeUndefined();
             let bbConcept = BBconceptExps.conceptRef?.referred;
             expect(bbConcept).not.toBeNull();
+            expect(bbConcept).not.toBeUndefined();
             // for each expression in the set, it should refer to a property of 'BB'
             BBconceptExps.exps.forEach(exp => {
                 expect(exp.referedElement.referred === bbConcept);
                 let prop = exp.appliedfeature?.referedElement?.referred;
                 expect(prop).not.toBeNull();
+                expect(prop).not.toBeUndefined();
                 expect(bbConcept.allProperties().includes(prop));
             });
         } else {
@@ -66,8 +69,10 @@ describe("Checking expression on referedElement", () => {
             let CCconceptExps = readTest.conceptExps.find(ce => ce.conceptRef.name === "CC");
             // set of expressions should refer to some concept or interface in the language
             expect(CCconceptExps).not.toBeNull();
+            expect(CCconceptExps).not.toBeUndefined();
             let zzConcept = language.findConcept("ZZ");
             expect(zzConcept).not.toBeNull();
+            expect(zzConcept).not.toBeUndefined();
             expect(zzConcept instanceof PiLimitedConcept);
             // for each expression in the set, it should refer to an predefined instance of 'ZZ'
             CCconceptExps.exps.forEach(exp => {
@@ -89,6 +94,7 @@ describe("Checking expression on referedElement", () => {
             // check expressions on DD
             let DDconceptExps = readTest.conceptExps.find(ce => ce.conceptRef.name === "DD");
             expect(DDconceptExps).not.toBeNull();
+            expect(DDconceptExps).not.toBeUndefined();
             // for each expression in the set, it should refer to a function
             DDconceptExps.exps.forEach(exp => {
                 expect(exp instanceof PiLangFunctionCallExp);
@@ -98,8 +104,35 @@ describe("Checking expression on referedElement", () => {
                 (exp as PiLangFunctionCallExp).actualparams.forEach(param => {
                     if (param.sourceName !== "container") {
                         expect(param.referedElement?.referred).not.toBeNull();
+                        expect(param.referedElement?.referred).not.toBeUndefined();
                     }
                 });
+            });
+        } else {
+            console.log("Language not present");
+        }
+    });
+
+    test("applied feature in FF", () => {
+        let expressionFile = testdir + "test1.pitest";
+        if (!!language) {
+            const readTest = new LanguageExpressionParser(language).parse(expressionFile);
+            // check expressions on FF
+            let FFconceptExps = readTest.conceptExps.find(ce => ce.conceptRef.name === "FF");
+            expect(FFconceptExps).not.toBeNull();
+            let ffConcept = FFconceptExps.conceptRef?.referred;
+            expect(ffConcept).not.toBeNull();
+            // the only expression in the set is an appliedFeature
+            // its reference should be set correctly
+            let aaConcept = language.findConcept("AA");
+            FFconceptExps.exps.forEach(exp => {
+                expect(exp instanceof PiLangSelfExp);
+                expect(exp.referedElement.referred === ffConcept);
+                let elem = exp.findRefOfLastAppliedFeature();
+                expect(elem).not.toBeNull();
+                expect(elem).not.toBeUndefined();
+                expect(elem.name === "aa");
+                expect(elem.type?.referred === aaConcept);
             });
         } else {
             console.log("Language not present");
