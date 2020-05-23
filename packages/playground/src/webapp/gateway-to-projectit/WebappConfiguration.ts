63c5d6f67461c13ed121bdfbe9f356251c2feded
Jos Warmer <jos.warmer@openmodeling.nl>
2020-05-23 21:14:41 +0200

    Back to correct example

--- packages/playground/src/webapp/gateway-to-projectit/WebappConfiguration.ts
+++ packages/playground/src/webapp/gateway-to-projectit/WebappConfiguration.ts
@@ -4,11 +4,11 @@ import { PiEnvironment } from "@projectit/core";
  * The one and only reference to the actual language for which this editor runs
  */
 
-import { ExampleEnvironment } from "../../example/environment/gen/ExampleEnvironment";
-export const editorEnvironment: PiEnvironment = ExampleEnvironment.getInstance();
+// import { ExampleEnvironment } from "../../example/environment/gen/ExampleEnvironment";
+// export const editorEnvironment: PiEnvironment = ExampleEnvironment.getInstance();
 
-// import { DemoEnvironment } from "../../demo/environment/gen/DemoEnvironment";
-// export const editorEnvironment: PiEnvironment = DemoEnvironment.getInstance();
+import { DemoEnvironment } from "../../demo/environment/gen/DemoEnvironment";
+export const editorEnvironment: PiEnvironment = DemoEnvironment.getInstance();
 
 // import { CalculatorEnvironment } from "../../calculator/environment/gen/CalculatorEnvironment";
 // export const editorEnvironment: PiEnvironment = CalculatorEnvironment.getInstance();
