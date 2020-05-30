356c0390d484cfd9e4e2aecee0fc11f7633ba25d
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-05-30 16:51:22 +0200

    added extra checks for scoper and change file name extension to .scope

--- packages/playground/src/webapp/gateway-to-projectit/WebappConfiguration.ts
+++ packages/playground/src/webapp/gateway-to-projectit/WebappConfiguration.ts
@@ -19,10 +19,11 @@ import { PiEnvironment } from "@projectit/core";
 // import { TaxRulesEnvironment } from "../../taxrules/environment/gen/TaxRulesEnvironment";
 // export const editorEnvironment: PiEnvironment = TaxRulesEnvironment.getInstance();
 
-import { DemoEnvironment } from "../../tutorial-language/environment/gen/DemoEnvironment";
-export const editorEnvironment: PiEnvironment = DemoEnvironment.getInstance();
+// import { DemoEnvironment } from "../../tutorial-language/environment/gen/DemoEnvironment";
+// export const editorEnvironment: PiEnvironment = DemoEnvironment.getInstance();
 
-// export const editorEnvironment: PiEnvironment = CalculatorEnvironment.getInstance();
+import { PiLanguageEnvironment } from "../../pi-language-definition-language/environment/gen/PiLanguageEnvironment";
+export const editorEnvironment: PiEnvironment = PiLanguageEnvironment.getInstance();
 
 // export const editorEnvironment: PiEnvironment = JsonEnvironment.getInstance();
 
