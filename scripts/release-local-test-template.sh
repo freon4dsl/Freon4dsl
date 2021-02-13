21cbec0762285bc16f0f82c02d4b18b2224b2288
jos <jos.warmer@openmodeling.nl>
2021-02-13 14:18:50 +0100

    Uograde to version 0.1.0

---
+++ scripts/release-local-test-template.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# start vedaccio as local npm registry in the background
+# If it is already running it will fail to start, that is ok
+verdaccio &
+
+# publish projectit
+lerna run build-release
+lerna run unpublish-local
+lerna run publish-local
+
+# checkout and build example project
+mkdir -p ../tmp
+cd ../tmp
+rm -rf ProjectIt-template
+git clone https://github.com/projectit-org/ProjectIt-template.git
+cd ProjectIt-template
+git checkout main
+yarn install --registry http://localhost:4873
+yarn generate
+yarn build
