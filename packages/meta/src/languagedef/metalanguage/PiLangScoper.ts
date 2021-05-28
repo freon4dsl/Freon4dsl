ab6f47e3c286d89ff2377d9483232ad67aacc093
Anneke Kleppe <anneke@warmerkleppe.nl>
2021-05-28 12:54:49 +0200

    addressed todo

--- packages/meta/src/languagedef/metalanguage/PiLangScoper.ts
+++ packages/meta/src/languagedef/metalanguage/PiLangScoper.ts
@@ -6,15 +6,8 @@ const anyElement = "_$anyElement";
 
 export class PiLangScoper {
     public language: PiLanguage;
-    // TODO make searchlist a map {owner, unitName}
-    private searchList: string[] = [];
 
     public getFromVisibleElements(owner: PiLangElement, name: string, typeName: string): PiLangElement {
-        // check whether we are already searching for a this unitName
-        if (this.searchingFor(name)) {
-            return null;
-        }
-
         let result: PiLangElement;
         if (typeName === "PiConcept" || typeName === "PiExpressionConcept" || typeName === "PiBinaryExpressionConcept") {
             result = this.language.findConcept(name);
@@ -38,33 +31,7 @@ export class PiLangScoper {
         } else {
             console.error("NO calculation found for " + name + ", owner: " + owner.name + ", type:" + typeName);
         }
-        this.cleanSearchList(name);
         return result;
     }
 
-    /**
-     * Returns true if a search is already in progress for 'unitName'
-     * @param name
-     */
-    private searchingFor(name?: string): boolean {
-        const myName: string = !!name ? name : anyElement;
-        if (this.searchList.includes(myName)) {
-            return true;
-        } else {
-            this.searchList.push(myName);
-        }
-        return false;
-    }
-
-    /**
-     * Removes the 'metatype' from the list of searches that are in progress
-     * @param name
-     */
-    private cleanSearchList(name?: string) {
-        const type: string = !!name ? name : anyElement;
-        const index = this.searchList.indexOf(type);
-        if (index > -1) {
-            this.searchList.splice(index, 1);
-        }
-    }
 }
