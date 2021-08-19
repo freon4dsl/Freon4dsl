deff187cc2581d39afdfcfe1aae2d4b46028fb78
jos <jos.warmer@openmodeling.nl>
2021-08-19 12:13:21 +0200

    Fix focussed element doies not show cursor ==> await tick() needed

--- packages/playground/src/webapp/main.ts
+++ packages/playground/src/webapp/main.ts
@@ -3,13 +3,17 @@ import App from './App.svelte';
 
 // Mute or unmute logs here (in addition to elsewhere).
 
-PiLogger.mute("PiEditor");
-PiLogger.mute("TextComponent");
+// PiLogger.mute("PiEditor");
+// PiLogger.mute("TextComponent");
 PiLogger.mute("AliasComponent");
 PiLogger.mute("AUTORUN");
 PiLogger.mute("AFTER_UPDATE");
 PiLogger.mute("SelectOptionList");
 PiLogger.mute("RenderComponent");
+PiLogger.mute("SelectableComponent");
+PiLogger.mute("DropdownComponent");
+PiLogger.mute("DropdownItemComponent");
+
 // PiLogger.muteAllLogs()
 
 const app = new App({
