343929d5370f1df3a83601c2ffd15709c18e3a46
Anneke Kleppe <anneke@warmerkleppe.nl>
2021-08-23 17:30:27 +0200

    adjusted test of parser to take into account the long unparsed sentence

--- packages/test/src/demo/__tests__/checkParser.test.ts
+++ packages/test/src/demo/__tests__/checkParser.test.ts
@@ -27,7 +27,7 @@ describe("Testing Parser", () => {
             const fileHandler = new FileHandler();
 
             // unparse the first unit to a string and write it to File
-            fileHandler.stringToFile(path, unparser.writeToString(model.models[0], 0));
+            fileHandler.stringToFile(path, unparser.writeToString(model.models[0], 0, false));
             // read it back in
             const unit1 = parser.readFromString(fileHandler.stringFromFile(path), "DemoModel") as DemoModel;
 
