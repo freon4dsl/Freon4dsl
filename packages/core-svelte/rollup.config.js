850babb8e75576a425e2900be0e9614c7dea11ef
jos <jos.warmer@openmodeling.nl>
2021-10-11 11:24:37 +0200

    Fix build

--- packages/core-svelte/rollup.config.js
+++ packages/core-svelte/rollup.config.js
@@ -83,8 +83,8 @@ export default {
 		commonjs(),
 		typescript({
 			sourceMap: !production,
-			inlineSources: !production,
-			tsconfig: "./tsconfig.json"
+			inlineSources: !production
+			// tsconfig: "./tsconfig.json"
 		}),
 
 		// If we're building for production (npm run build
