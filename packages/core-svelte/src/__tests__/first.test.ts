b4854c73fa971a9f67bb8b2308f2cb9902c3ac7f
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-02-16 09:53:52 +0100

    added tests to core_svelte

---
+++ packages/core-svelte/src/__tests__/first.test.ts
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/svelte';
+import { TextComponent } from "../components";
+import { PiEditor, TextBox } from "@projectit/core";
+
+// see https://testing-library.com/docs/ for testing options
+
+describe("First test in core-svelte", () => {
+
+    test("see if test is starting", () => {
+        expect(true).not.toBe(false);
+    });
+
+    test("says 'hello world!'", () => {
+        const myTextBox = new TextBox(null, "role", () => "Hello world!", (v: string) => {
+            return;
+        });
+        const myEditor = new PiEditor(null, null);
+        render(TextComponent, {textBox: myTextBox, editor: myEditor });
+        const node = screen.queryByText("Hello world!");
+        expect(node).not.toBeNull();
+    });
+
+
+});
