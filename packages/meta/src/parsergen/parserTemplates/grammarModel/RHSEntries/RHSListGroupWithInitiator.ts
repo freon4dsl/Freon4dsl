cd9997451737891b85da2bfefcb7fcf0a2d304f4
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-02-17 11:32:16 +0100

    fixed parsing initiator for lists

--- packages/meta/src/parsergen/parserTemplates/grammarModel/RHSEntries/RHSListGroupWithInitiator.ts
+++ packages/meta/src/parsergen/parserTemplates/grammarModel/RHSEntries/RHSListGroupWithInitiator.ts
@@ -3,6 +3,7 @@ import { RHSPropEntry } from "./RHSPropEntry";
 import { PiProperty } from "../../../../languagedef/metalanguage";
 import { internalTransformNode, ParserGenUtil } from "../../ParserGenUtil";
 import { makeIndent } from "../GrammarUtils";
+import { ParseHelpers } from "@projectit/playground/dist/utils/ParseHelpers";
 
 export class RHSListGroupWithInitiator extends RHSPropPartWithSeparator {
     // `("joinText" propTypeName)*`
@@ -19,13 +20,30 @@ export class RHSListGroupWithInitiator extends RHSPropPartWithSeparator {
     }
 
     toMethod(index: number, nodeName: string, mainAnalyserName: string): string {
-        return `// RHSListGroupWithInitiator  
-            if (!${nodeName}[${index}].isEmptyMatch) {          
-                ${ParserGenUtil.internalName(this.property.name)} = [];
-                for (const subNode of ${nodeName}[${index}].nonSkipChildren.toArray()) {
-                    ${ParserGenUtil.internalName(this.property.name)}.push(this.${mainAnalyserName}.${internalTransformNode}(this.${mainAnalyserName}.getGroup(subNode).nonSkipChildren.toArray()[1]));
+        return `
+        // RHSListGroupWithInitiator
+        if (!${nodeName}[${index}].isEmptyMatch) {
+            ${ParserGenUtil.internalName(this.property.name)} = [];
+            const group = this.${mainAnalyserName}.getGroup(${nodeName}[${index}]);
+            if (group !== ${nodeName}[${index}]) {
+                for (const child of ${nodeName}[${index}].nonSkipChildren.toArray()) {
+                    ${ParserGenUtil.internalName(this.property.name)}.push(this.${mainAnalyserName}.${internalTransformNode}(child.nonSkipChildren.toArray()[1]));
                 }
-            }`;
+            } else {
+                for (const child of ${nodeName}) {
+                    ${ParserGenUtil.internalName(this.property.name)}.push(this.${mainAnalyserName}.${internalTransformNode}(child.nonSkipChildren.toArray()[1]));
+                }
+            }
+        } // end RHSListGroupWithInitiator
+        `;
+        // return `
+        //     // RHSListGroupWithInitiator
+        //     if (!${nodeName}[${index}].isEmptyMatch) {
+        //         ${ParserGenUtil.internalName(this.property.name)} = [];
+        //         for (const subNode of ${nodeName}[${index}].nonSkipChildren.toArray()) {
+        //             ${ParserGenUtil.internalName(this.property.name)}.push(this.${mainAnalyserName}.${internalTransformNode}(this.${mainAnalyserName}.getGroup(subNode).nonSkipChildren.toArray()[1]));
+        //         }
+        //     }`;
     }
 
     toString(depth: number): string {
