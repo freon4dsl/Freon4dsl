080a5c6f445190a7b439f32b9fbebfe720781a03
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-03-12 17:52:46 +0100

    tmp commit: unit of measurements still not working

--- packages/playground/src/typer-test/__tests__/testTyper.test.ts
+++ packages/playground/src/typer-test/__tests__/testTyper.test.ts
@@ -1,10 +1,11 @@
-import { GenericModelSerializer } from "@projectit/core";
+import { GenericModelSerializer, PiError } from "@projectit/core";
 import { FileHandler } from "../../octopus/__tests__/FileHandler";
+import { TyTest, TyTestModelUnitType, UnitA, UnitB } from "../language/gen";
 import { TyTestEnvironment } from "../environment/gen/TyTestEnvironment";
-import { TyTestModelUnitType, UnitA, UnitB, UnitC } from "../language/gen";
 
 const writer = TyTestEnvironment.getInstance().writer;
 const reader = TyTestEnvironment.getInstance().reader;
+const validator = TyTestEnvironment.getInstance().validator;
 const serial: GenericModelSerializer = new GenericModelSerializer();
 const handler = new FileHandler();
 const metatype: string = "UnitA";
@@ -12,10 +13,11 @@ const testdir = "src/typer-test/__inputs__/";
 
 function compareReadAndWrittenFiles(path: string) {
     try {
-        const unit1 = reader.readFromString(handler.stringFromFile(path), metatype) as TyTestModelUnitType;
+        const model = new TyTest();
+        const unit1 = reader.readFromString(handler.stringFromFile(path), metatype, model) as TyTestModelUnitType;
         let result: string = writer.writeToString(unit1, 0, false);
         expect(result.length).toBeGreaterThan(0);
-        const unit2 = reader.readFromString(result, metatype);
+        const unit2 = reader.readFromString(result, metatype, model);
         // simply comparing the units does not work because the id properties of the two units
         // are not the same, therefore we use the hack of checking whether both units in JSON
         // format are the same
@@ -31,24 +33,89 @@ function compareReadAndWrittenFiles(path: string) {
 describe ("Testing Typer Ideas", () => {
 
     test ("read types", () => {
-        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "myTypeDefinitions.types"), "UnitB") as UnitB;
+        const model = new TyTest();
+        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "myTypeDefinitions.types"), "UnitB", model) as UnitB;
         expect(unit1).not.toBeNull();
         expect(unit1.typeDefs.length).not.toBe(0);
     });
 
-    test ("read expressions", () => {
-        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "simpleLines.expr"), "UnitA") as UnitA;
+    test ("literal expressions", () => {
+        const model = new TyTest();
+        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "literals.expr"), "UnitA", model) as UnitA;
         expect(unit1).not.toBeNull();
         if (!!unit1) {
-            console.log(writer.writeToString(unit1));
+            const errors: PiError[] = validator.validate(unit1);
+            expect(errors.length).toBe(6);
+            expect(errors.find(e => e.message === "Type NUMBER of [456] is not equal to STRING")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type NUMBER of [456] is not equal to BOOLEAN")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type STRING of [\"string\"] is not equal to NUMBER")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type STRING of [\"string\"] is not equal to BOOLEAN")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type BOOLEAN of [true] is not equal to STRING")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type BOOLEAN of [true] is not equal to NUMBER")).toBeTruthy();
+
+            // console.log(errors.map(e => e.message).join("\n"));
         }
     });
 
-    test ("read unitC", () => {
-        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "simpleLines.tmp"), "UnitC") as UnitC;
+    test ("literal expressions with complex types", () => {
+        const model = new TyTest();
+        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "literalsWithComplexTypes.expr"), "UnitA", model) as UnitA;
         expect(unit1).not.toBeNull();
         if (!!unit1) {
-            console.log(writer.writeToString(unit1));
+            const errors: PiError[] = validator.validate(unit1);
+            expect(errors.length).toBe(10);
+            expect(errors.find(e => e.message === "Type NUMBER of [12] is not equal to kWh")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type NUMBER of [456] is not equal to Collection < Grams >")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type STRING of [\"string\"] is not equal to Set < Bag < Hours > >")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type BOOLEAN of [true] is not equal to Meters")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type NUMBER of [100] is not equal to Set < BOOLEAN >")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type STRING of [\"string\"] is not equal to Bag < Set < NUMBER > >")).toBeTruthy();
+
+            // console.log(errors.map(e => e.message).join("\n"));
+        }
+    });
+
+    test ("complex expressions with simple types", () => {
+        const model = new TyTest();
+        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "complexExpWithSimpleTypes.expr"), "UnitA", model) as UnitA;
+        expect(unit1).not.toBeNull();
+        if (!!unit1) {
+            const errors: PiError[] = validator.validate(unit1);
+            expect(errors.length).toBe(2);
+            expect(errors.find(e => e.message === "Type Set < BOOLEAN > of [Set { true, true, false }] is not equal to BOOLEAN")).toBeTruthy();
+            expect(errors.find(e => e.message === "Type Bag < Set < NUMBER > > of [Bag { Set { 12, 13, 14 }, Sequence { \"string\", \"Str\", \"STRING\" } }] is not equal to NUMBER")).toBeTruthy();
+
+            // console.log(errors.map(e => e.message).join("\n"));
+        }
+    });
+
+    test ("complex expressions with complex types", () => {
+        const model = new TyTest();
+        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "complexExpWithComplexTypes.expr"), "UnitA", model) as UnitA;
+        expect(unit1).not.toBeNull();
+        if (!!unit1) {
+            const errors: PiError[] = validator.validate(unit1);
+            // expect(errors.length).toBe(10);
+            // expect(errors.find(e => e.message === "Type of [456] should equal STRING")).toBeTruthy();
+            // expect(errors.find(e => e.message === "Type of [456] should equal BOOLEAN")).toBeTruthy();
+            // expect(errors.find(e => e.message === "Type of [\"string\"] should equal NUMBER")).toBeTruthy();
+            // expect(errors.find(e => e.message === "Type of [\"string\"] should equal BOOLEAN")).toBeTruthy();
+            // expect(errors.find(e => e.message === "Type of [true] should equal STRING")).toBeTruthy();
+            // expect(errors.find(e => e.message === "Type of [true] should equal STRING")).toBeTruthy();
+
+            console.log(errors.map(e => e.message).join("\n"));
+        }
+    });
+
+    test ("expressions with correct types", () => {
+        const model = new TyTest();
+        const unit1 = reader.readFromString(handler.stringFromFile(testdir + "correctExps.expr"), "UnitA", model) as UnitA;
+        expect(unit1).not.toBeNull();
+        if (!!unit1) {
+            const errors: PiError[] = validator.validate(unit1);
+            // expect(errors.length).toBe(0);
+
+            console.log(errors.map(e => e.message).join("\n"));
         }
     });
 });
