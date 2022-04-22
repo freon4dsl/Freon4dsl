63118454c797039dd58cb8dcfc201e96ac7f57df
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-04-22 13:33:24 +0200

    cleaned up old documentations tags

--- packages/core/src/language/PiNamedElement.ts
+++ packages/core/src/language/PiNamedElement.ts
@@ -1,7 +1,5 @@
-// tag::named-element-interface[]
 import { PiElement } from "./PiElement";
 
 export interface PiNamedElement extends PiElement {
     name: string;
 }
-// end::named-element-interface[]
