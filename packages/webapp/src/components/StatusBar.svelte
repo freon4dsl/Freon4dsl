f0ca6730db28d0015234ac4f5434ddcc868fc85d
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-05-07 17:41:04 +0200

    adjusted layout in statusbar

--- packages/webapp/src/components/StatusBar.svelte
+++ packages/webapp/src/components/StatusBar.svelte
@@ -5,9 +5,9 @@
 	// import { editorEnvironment } from "../WebappConfiguration";
 	import { currentModelName, currentUnitName } from "../stores/ModelStore";
 	import { modelErrors } from "../stores/ValidationStore";
-	import { mdiCheckCircle, mdiChevronRight, mdiAlertCircleCheck, mdiWeb } from '@mdi/js';
+	import { mdiCheckCircle, mdiChevronRight, mdiAlertCircle } from '@mdi/js';
 	import { Svg } from '@smui/common/elements';
-	import { Label, Icon } from '@smui/common';
+	import { Icon } from '@smui/common';
 	import IconButton from '@smui/button';
 
 	// let currentBox: Box = null;
@@ -17,50 +17,45 @@
 	// });
 </script>
 
-<div class="status-bar">
-
-	{#if $modelErrors.length > 0}
-			<IconButton>
-				<Icon component={Svg} viewBox='0 0 24 24'>
-					<path fill='currentColor' d={mdiAlertCircleCheck} />
-				</Icon>
-			</IconButton>
-
-	{:else}
-		<IconButton>
+<span class="status-bar">
+	<IconButton class="less-padding">
+		{#if $modelErrors.length > 0}
 			<Icon component={Svg} viewBox='0 0 24 24'>
-				<path fill='currentColor' d={mdiCheckCircle} />
+				<path d={mdiAlertCircle}/>
 			</Icon>
-		</IconButton>
-
-	{/if}
-
+		{:else}
+			<Icon component={Svg} viewBox='0 0 24 24'>
+				<path d={mdiCheckCircle}/>
+			</Icon>
+		{/if}
+	</IconButton>
 	<i class='mdc-typography--body2'>{$currentModelName}</i>
+	<IconButton>
 	<Icon component={Svg} viewBox="0 0 24 24">
-		<path fill="currentColor" d={mdiChevronRight} />
+		<path d={mdiChevronRight}/>
 	</Icon>
+		</IconButton>
 	<i class='mdc-typography--body2'>{$currentUnitName}</i>
-<!--	box: <i>{currentBox?.role} {currentBox?.$id}</i>-->
-<!--	kind: <i>{currentBox?.kind} </i>-->
-<!--	elem: <i>{currentBox?.element?.piId()} - {currentBox?.element?.piLanguageConcept()} </i>-->
-<!--	(x, y): <i>{(!!currentBox ? Math.round(currentBox.actualX + editorEnvironment.editor.scrollX)-->
-<!--	+ ", " + Math.round(currentBox?.actualY + editorEnvironment.editor.scrollY): "NAN")}-->
-<!--	"{(isTextBox(currentBox) ? currentBox.getText() : "NotTextBox" )}"</i>-->
-</div>
+	<!--	box: <i>{currentBox?.role} {currentBox?.$id}</i>-->
+	<!--	kind: <i>{currentBox?.kind} </i>-->
+	<!--	elem: <i>{currentBox?.element?.piId()} - {currentBox?.element?.piLanguageConcept()} </i>-->
+	<!--	(x, y): <i>{(!!currentBox ? Math.round(currentBox.actualX + editorEnvironment.editor.scrollX)-->
+	<!--	+ ", " + Math.round(currentBox?.actualY + editorEnvironment.editor.scrollY): "NAN")}-->
+	<!--	"{(isTextBox(currentBox) ? currentBox.getText() : "NotTextBox" )}"</i>-->
+</span>
 
-
-<style>
-    .status-bar {
-        position: relative;
-        /*height: var(--pi-statusbar-height);*/
-				height: 36px;
-        width: 100%;
-        /*color: var(--theme-colors-color);*/
-        /*background: var(--theme-colors-inverse_color);*/
-        /*font-size: var(--pi-button-font-size);*/
-        /*border-bottom: var(--theme-colors-list_divider) solid 1px;*/
-        box-sizing: border-box;
-				display: flex;
-				padding-top: 8px;
-    }
+<style lang="scss">
+	.status-bar {
+		position: relative;
+		height: 36px;
+		width: 100%;
+		/*height: var(--pi-statusbar-height);*/
+		/*color: var(--theme-colors-color);*/
+		/*background: var(--theme-colors-inverse_color);*/
+		/*font-size: var(--pi-button-font-size);*/
+		/*border-bottom: var(--theme-colors-list_divider) solid 1px;*/
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+	}
 </style>
