4b32394de0f7688b1713a2b0c71893cba32362e2
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-06-01 15:09:05 +0200

    added playground project to be used in documentation

--- packages/playground/src/docu-project/__tests__/readInput.test.ts
+++ packages/playground/src/docu-project/__tests__/readInput.test.ts
@@ -1,21 +1,21 @@
 import { GenericModelSerializer, PiModelUnit } from "@projectit/core";
 import { FileHandler } from "../../utils/FileHandler";
 import { DocuProjectEnvironment } from "../config/gen/DocuProjectEnvironment";
-import { InsuranceModel } from "../language/gen";
+import { InsuranceModel, Part, Product } from "../language/gen";
 
 const writer = DocuProjectEnvironment.getInstance().writer;
 const reader = DocuProjectEnvironment.getInstance().reader;
 const validator = DocuProjectEnvironment.getInstance().validator;
-const serial: GenericModelSerializer = new GenericModelSerializer();
 const handler = new FileHandler();
 
 function addPartToModel(model: InsuranceModel, filepath: string) {
     try {
         const langSpec: string = handler.stringFromFile(filepath);
-        const unit1 = reader.readFromString(langSpec, "Part", model) as PiModelUnit;
+        const unit1 = reader.readFromString(langSpec, "Part", model) as Part;
+        unit1.name = filepath.split("/").pop().split(".").shift();
     } catch (e) {
         console.log(e.message + e.stack);
-        // expect(e).toBeNaN();
+        expect(e).toBeNull();
     }
 }
 
@@ -23,27 +23,32 @@ function addProductToModel(model: InsuranceModel, filepath: string) {
     try {
         const langSpec: string = handler.stringFromFile(filepath);
         const unit1 = reader.readFromString(langSpec, "Product", model) as PiModelUnit;
-        unit1.name = "unit1";
+        unit1.name = filepath.split("/").pop().split(".").shift();
     } catch (e) {
         console.log(e.message + e.stack);
-        // expect(e).toBeNaN();
+        // expect(e).toBeNull();
     }
 }
 
 describe("Testing DocuProject", () => {
     const model: InsuranceModel = new InsuranceModel();
+    model.name = "TEST_MODEL";
 
-    test("add first part", () => {
-        addPartToModel(model, "src/docu-project/__inputs__/Part1.part");
+    test("add health base", () => {
+        addPartToModel(model, "src/docu-project/__inputs__/base/Health.base");
     });
 
-    test("add second part", () => {
-        addPartToModel(model, "src/docu-project/__inputs__/Part2.part");
+    test("add home base", () => {
+        addPartToModel(model, "src/docu-project/__inputs__/base/Home.base");
     });
 
-    test("add first base product", () => {
-        addProductToModel(model, "src/docu-project/__inputs__/Base1.prod");
-        console.log(model.getUnits().map(u => u.name).join("\n\n"));
+    test("add legal base", () => {
+        addPartToModel(model, "src/docu-project/__inputs__/base/Legal.base");
+    });
+
+    test("add product", () => {
+        addProductToModel(model, "src/docu-project/__inputs__/products/HealthAll.prod");
+        console.log(model.getUnits().map(u => u instanceof Part ? u.part.name : u instanceof Product ? u.product.name : "no unit").join("\n\n"));
         const errors = validator.validate(model);
         console.log(errors.map(e => e.message + ' in [' + e.locationdescription + ']').join("\n"));
     });
