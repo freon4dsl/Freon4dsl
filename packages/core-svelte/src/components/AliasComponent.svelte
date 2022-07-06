d486b6010f868cb452cecb57e32e9f17a27af851
Jos Warmer <jos.warmer@openmodeling.nl>
2022-07-06 11:13:12 +0200

    Fix error in click_outside

--- packages/core-svelte/src/components/AliasComponent.svelte
+++ packages/core-svelte/src/components/AliasComponent.svelte
@@ -304,7 +304,7 @@
 
     });
 
-    const handleClickOutside = (event: Event): void => {
+    const handleClickOutside = (): void => {
         setOpen("clickOutside", false);
         isEditing = false;
     };
