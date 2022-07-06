c43c486e3a2350ec99138364e399ff99e9eb2646
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-07-06 09:34:08 +0200

    added tests on Label component

---
+++ packages/core-svelte/src/__tests__/indent-component/IndentComponent.test.ts
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from "@testing-library/svelte";
+import { IndentBox, LabelBox, PiEditor, PiElement } from "@projectit/core";
+import { SimpleElement } from "../models/one-element/SimpleElement";
+import IndentComponent from "../../components/IndentComponent.svelte";
+
+describe("Indent component", () => {
+    // an indent box coupled to element2, that takes as slot:
+    // a label box coupled to element1
+    const element1: PiElement = new SimpleElement("WHATSINANAME");
+    const element2: PiElement = new SimpleElement("ANOTHERNAME");
+    const myEditor = new PiEditor(null, null);
+    const myLabelBox = new LabelBox(element1, "role", () => "LabelText");
+    const myIndentBox = new IndentBox(element2,"indent-role", 4, myLabelBox);
+
+    it(", with label as slot, is indented", () => {
+        const result = render(IndentComponent, { indentBox: myIndentBox, editor: myEditor });
+        const myLabel = screen.getByText('LabelText');
+        expect(myLabel).toBeVisible();
+        const indentComp = screen.getByTestId(`${myIndentBox.element.piId()}-${myIndentBox.role}`);
+        // Cannot test the dynamic value of margin-left TODO find out how to
+        // For now, we test the static value of margin-left
+        expect(indentComp).toHaveStyle('margin-left: 50px');
+        // and ... the dynamic value of 'style' in the Svelte component
+        // Note, that 'style' needs to be exported from IndentComponent for this to work.
+        const xxx = result.component;
+        expect(xxx.style).toBe("32px;");
+    });
+
+});
