76d3af028580da643b96441f54a7b885e978b56c
Jos Warmer <jos.warmer@openmodeling.nl>
2022-07-10 20:28:50 +0200

    Start of interpreter generator

--- packages/meta/src/interpretergen/InterpreterGenerator.ts
+++ packages/meta/src/interpretergen/InterpreterGenerator.ts
@@ -1,6 +1,11 @@
 import * as fs from "fs";
 import { PiLanguage } from "../languagedef/metalanguage";
-import { GenerationStatus, FileUtil, DIAGRAM_FOLDER, MetaLogger, DIAGRAM_GEN_FOLDER } from "../utils";
+import {
+    GenerationStatus,
+    FileUtil,
+    MetaLogger,
+    INTERPRETER_FOLDER, INTERPRETER_GEN_FOLDER
+} from "../utils";
 import { InterpreterBaseTemplate } from "./templates/InterpreterBaseTemplate";
 
 const LOGGER = new MetaLogger("InterpreterGenerator").mute();
@@ -10,31 +15,27 @@ const LOGGER = new MetaLogger("InterpreterGenerator").mute();
 export class InterpreterGenerator {
     public outputfolder: string = ".";
     public language: PiLanguage;
-    private diagramFolder: string;
-    private diagramGenFolder: string;
+    private interpreterFolder: string;
+    private interpreterGenFolder: string;
     fileNames: string[] = [];
-    private diagramAstFolder: string;
 
     generate(): void {
         if (this.language == null) {
-            LOGGER.error("Cannot generate diagrams because language is not set.");
+            LOGGER.error("Cannot generate interpreter because language is not set.");
             return;
         }
         const generationStatus = new GenerationStatus();
         this.getFolderNames();
-        LOGGER.log("Generating diagrams in folder " + this.diagramFolder + " for language " + this.language?.name);
+        LOGGER.log("Generating interpreter in folder " + this.interpreterFolder + " for language " + this.language?.name);
 
         const htmlTemplate = new InterpreterBaseTemplate(true);
 
         // Prepare folders
-        FileUtil.createDirIfNotExisting(this.diagramGenFolder);
-        FileUtil.deleteFilesInDir(this.diagramGenFolder, generationStatus);
+        FileUtil.createDirIfNotExisting(this.interpreterGenFolder);
+        FileUtil.deleteFilesInDir(this.interpreterGenFolder, generationStatus);
 
-        // make title
-        let title: string = `Class diagram for language ${this.language.name}`;
-
-        //  Generate the html version of the complete diagram
-        let generatedFilePath = `${this.diagramGenFolder}/complete-view.html`;
+        // TODO Change this for interpreter
+        let generatedFilePath = `${this.interpreterGenFolder}/complete-view.html`;
         let generatedContent  = htmlTemplate.interpreterBase(this.language);
         this.makeFile(`complete diagram in html`, generatedFilePath, generatedContent, generationStatus);
 
@@ -46,18 +47,13 @@ export class InterpreterGenerator {
     }
 
     private getFolderNames() {
-        this.diagramFolder = this.outputfolder + "/" + DIAGRAM_FOLDER;
-        this.diagramGenFolder = this.outputfolder + "/" + DIAGRAM_GEN_FOLDER;
-        this.diagramAstFolder = this.outputfolder + "/" + DIAGRAM_GEN_FOLDER + "/ast";
+        this.interpreterFolder = this.outputfolder + "/" + INTERPRETER_FOLDER;
+        this.interpreterGenFolder = this.outputfolder + "/" + INTERPRETER_GEN_FOLDER;
     }
 
     clean(force: boolean) {
         this.getFolderNames();
-        FileUtil.deleteDirAndContent(this.diagramAstFolder);
-        FileUtil.deleteDirIfEmpty(this.diagramAstFolder);
-        FileUtil.deleteDirAndContent(this.diagramGenFolder);
-        FileUtil.deleteDirIfEmpty(this.diagramGenFolder);
-        FileUtil.deleteDirAndContent(this.diagramFolder);
-        FileUtil.deleteDirIfEmpty(this.diagramFolder);
+        FileUtil.deleteDirAndContent(this.interpreterGenFolder);
+        FileUtil.deleteDirIfEmpty(this.interpreterFolder);
     }
 }
