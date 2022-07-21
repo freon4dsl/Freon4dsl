36fed9fa871b9cc75bab434c2fcce94e4bd60663
Jos Warmer <jos.warmer@openmodeling.nl>
2022-07-21 10:11:17 +0200

    Add Save command to save copy of playgropund language to samples

--- packages/tools/src/InstallLanguage.ts
+++ packages/tools/src/InstallLanguage.ts
@@ -4,9 +4,11 @@ import { ProjectItGenerateLanguage } from "@projectit/meta/dist/commandline/Proj
 
 import { CommandLineFlagParameter, CommandLineParser } from "@rushstack/ts-command-line";
 import { CopyLanguage } from "./CopyLanguage";
+import { SaveLanguage } from "./SaveLanguage";
 
 export class InstallLanguage extends CommandLineParser {
     private languageAction: CopyLanguage;
+    private saveAction: SaveLanguage;
 
     private verboseArg: CommandLineFlagParameter;
 
@@ -16,8 +18,9 @@ export class InstallLanguage extends CommandLineParser {
             toolDescription: "ProjectIt toolset for installing languages in playground."
         });
         this.languageAction = new CopyLanguage();
+        this.saveAction = new SaveLanguage();
         this.addAction(this.languageAction);
-
+        this.addAction(this.saveAction);
     }
 
     protected onDefineParameters(): void {
