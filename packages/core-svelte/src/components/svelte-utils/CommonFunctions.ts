8bf463c03b7125833293732f626fb16694ec6aee
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-10-11 13:37:35 +0200

    Created a function to set focus and scroll the element into view, but not clear where to use it. The selection mechanism needs to be settled first.

--- packages/core-svelte/src/components/svelte-utils/CommonFunctions.ts
+++ packages/core-svelte/src/components/svelte-utils/CommonFunctions.ts
@@ -1,6 +1,28 @@
 import type { Box } from "@projectit/core";
 import { PI_NULL_COMMAND, PiCommand, PiEditor, PiEditorUtil, PiPostAction, toPiKey } from "@projectit/core";
 import { runInAction } from "mobx";
+import { viewport } from "./EditorViewportStore";
+import { get } from "svelte/store";
+
+export function focusAndScrollIntoView(element: HTMLElement) {
+    if (!!element) {
+        element.focus();
+        // check if the element is already within the editor viewport
+        let rect = element.getBoundingClientRect();
+
+        let elemIsVisible = (
+            rect.top >= get(viewport).top &&
+            rect.left >= get(viewport).left &&
+            rect.bottom <= get(viewport).height &&
+            rect.right <= get(viewport).width
+        );
+
+        // if the element is not visible then scroll to it
+        if (!elemIsVisible) {
+            element.scrollIntoView();
+        }
+    }
+}
 
 export function executeCustomKeyboardShortCut(event: KeyboardEvent, index: number, box: Box, editor: PiEditor) {
     const cmd: PiCommand = PiEditorUtil.findKeyboardShortcutCommand(toPiKey(event), box, editor);
