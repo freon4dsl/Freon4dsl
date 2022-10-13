407d142a55519c1d2108f3a8c77a61d285cfb6a0
Jos Warmer <jos.warmer@openmodeling.nl>
2022-10-13 16:05:58 +0200

    make contents of ElementBox observable. like all children of boxes.

--- packages/core/src/editor/boxes/ElementBox.ts
+++ packages/core/src/editor/boxes/ElementBox.ts
@@ -1,9 +1,18 @@
+import { makeObservable, observable } from "mobx";
+import { PiElement } from "../../ast/index";
 import { Box } from "./Box";
 import { EmptyLineBox } from "./EmptyLineBox";
 
 export class ElementBox extends Box {
-    content: Box;
     kind: string = 'ElementBox';
+    content: Box = null;
+    
+    constructor(element: PiElement, role: string) {
+        super(element, role);
+        makeObservable(this, {
+            content: observable
+        })
+    }
 }
 
 export function isElementBox(b: Box): b is EmptyLineBox {
