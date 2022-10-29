3dcde75984791b8ab89fa0c0eeb79c96ffb2c858
Jos Warmer <jos.warmer@openmodeling.nl>
2022-10-29 16:24:25 +0200

    New example interpreter

--- packages/samples/Example/interpreter/ExampleInterpreter.ts
+++ packages/samples/Example/interpreter/ExampleInterpreter.ts
@@ -1,44 +1,38 @@
 // Generated my Freon once, will NEVER be overwritten.
 import { InterpreterContext, IMainInterpreter, RtObject, RtBoolean, RtNumber } from "@projectit/core";
-import {
-    BooleanLiteralExpression,
-    EqualsExpression,
-    NumberLiteralExpression,
-    PlusExpression
-} from "../language/gen/index";
+import { BooleanLiteralExpression, EqualsExpression, NumberLiteralExpression, PlusExpression } from "../language/gen/index";
 import { ExampleInterpreterBase } from "./gen/ExampleInterpreterBase";
+import { ExampleInterpreterContext, IExampleMainInterpreter } from "./gen/ExampleInterpreterInterpreterTypes";
 
-let main: IMainInterpreter;
+let main: IExampleMainInterpreter;
 
 /**
  * The class containing all interpreter functions twritten by thge language engineer.
  * This class is initially empty,  and will not be overwritten if it already exists..
  */
 export class ExampleInterpreter extends ExampleInterpreterBase {
-
-    constructor(m: IMainInterpreter) {
+    constructor(m: IExampleMainInterpreter) {
         super();
         main = m;
     }
 
-    evalBooleanLiteralExpression(node: BooleanLiteralExpression, ctx: InterpreterContext): RtObject {
+    evalBooleanLiteralExpression(node: BooleanLiteralExpression, ctx: ExampleInterpreterContext): RtObject {
         return RtBoolean.of(node.value);
     }
 
-    evalNumberLiteralExpression(node: NumberLiteralExpression, ctx: InterpreterContext): RtObject {
+    evalNumberLiteralExpression(node: NumberLiteralExpression, ctx: ExampleInterpreterContext): RtObject {
         return new RtNumber(node.value);
     }
 
-    evalPlusExpression(node: PlusExpression, ctx: InterpreterContext): RtObject {
+    evalPlusExpression(node: PlusExpression, ctx: ExampleInterpreterContext): RtObject {
         const left = main.evaluate(node.left, ctx);
         const right = main.evaluate(node.right, ctx);
-        return (left as RtNumber).plus(right as RtNumber );
+        return (left as RtNumber).plus(right as RtNumber);
     }
 
-    evalEqualsExpression(node: EqualsExpression, ctx: InterpreterContext): RtObject {
+    evalEqualsExpression(node: EqualsExpression, ctx: ExampleInterpreterContext): RtObject {
         const left = main.evaluate(node.left, ctx);
         const right = main.evaluate(node.right, ctx);
-        return (left).equals(right);
+        return left.equals(right);
     }
-
 }
