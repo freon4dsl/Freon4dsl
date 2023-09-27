80ce2365965736256e2a8a0ce5f7a2097b032890
Jos Warmer <jos.warmer@openmodeling.nl>
2023-09-27 21:51:37 +0200

    Render component, mute log

--- packages/playground/src/webapp/components/stores/UserMessageStore.ts
+++ packages/playground/src/webapp/components/stores/UserMessageStore.ts
@@ -3,7 +3,7 @@ import type { Writable } from "svelte/store";
 import { FreErrorSeverity } from "@freon4dsl/core";
 
 // info about Freon
-export const versionNumber = "0.6.0-beta.1";
+export const versionNumber = "0.6.0-beta.2";
 
 export let severity: Writable<string> = writable<string>(FreErrorSeverity.Error);
 export let userMessage: Writable<string> = writable<string>("This is an important message. Once you've read it, you can dismiss it.");
