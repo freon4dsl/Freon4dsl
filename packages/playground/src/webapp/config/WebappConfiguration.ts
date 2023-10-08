81fa56bc4325f34b36cc02e6a280d00f11ff52c6
Jos Warmer <jos.warmer@openmodeling.nl>
2023-10-08 19:04:56 +0200

    rename duplicate ListUtil to ArrayUtil, remove some logging

--- packages/playground/src/webapp/config/WebappConfiguration.ts
+++ packages/playground/src/webapp/config/WebappConfiguration.ts
@@ -1,22 +1,15 @@
 import { ServerCommunication } from "@freon4dsl/core";
-import type { FreEnvironment, FreErrorSeverity, IServerCommunication } from "@freon4dsl/core";
-import { setUserMessage } from "../components/stores/UserMessageStore";
+import type { FreEnvironment, IServerCommunication } from "@freon4dsl/core";
 import { LanguageInitializer } from "../language/LanguageInitializer";
-
 /**
  * The one and only reference to the actual language for which this editor runs
  */
-import { ExampleEnvironment } from "../../Example/config/gen/ExampleEnvironment";
-export const editorEnvironment: FreEnvironment = ExampleEnvironment.getInstance();
+import { LionCoreM3Environment } from "../../lionwebM3/config/gen/LionCoreM3Environment";
+export const editorEnvironment: FreEnvironment = LionCoreM3Environment.getInstance();
 LanguageInitializer.initialize();
 
 /**
  * The one and only reference to the server on which the models are stored
  */
 export const serverCommunication: IServerCommunication = ServerCommunication.getInstance();
-// Initialize server:
-serverCommunication.onError = (msg: string, severity: FreErrorSeverity): void => {
-    setUserMessage(msg, severity);
-}
-// serverCommunication.
 // export const serverCommunication: IServerCommunication = MpsServerCommunication.getInstance();
