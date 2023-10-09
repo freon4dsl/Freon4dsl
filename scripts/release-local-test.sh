3fb765f2c10c0461814f4e147a5e0b5852fb8e29
Jos Warmer <jos.warmer@openmodeling.nl>
2023-10-09 21:04:37 +0200

    Version to 0.6.0-beta.4

--- scripts/release-local-test.sh
+++ scripts/release-local-test.sh
@@ -16,7 +16,7 @@ cd ../tmp
 rm -rf Freon-example
 git clone https://github.com/freon4dsl/Freon-example.git
 cd Freon-example
-git checkout 0.6.0-beta.3
+git checkout 0.6.0-beta.4
 yarn install-local
 yarn generate
 # yarn build
