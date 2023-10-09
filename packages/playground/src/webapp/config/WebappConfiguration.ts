e3a697a55bd12b866367dd774fa1c7432fdc34d0
Jos Warmer <jos.warmer@openmodeling.nl>
2023-10-09 13:44:41 +0200

    new language

--- packages/playground/src/webapp/config/WebappConfiguration.ts
+++ packages/playground/src/webapp/config/WebappConfiguration.ts
@@ -1,12 +1,12 @@
-import { ServerCommunication } from "@freon4dsl/core";
 import type { FreEnvironment, IServerCommunication } from "@freon4dsl/core";
 import { LanguageInitializer } from "../language/LanguageInitializer";
+import { ServerCommunication } from "@freon4dsl/core";
 
 /**
  * The one and only reference to the actual language for which this editor runs
  */
-import { EducationEnvironment } from "../../Education/config/gen/EducationEnvironment";
-export const editorEnvironment: FreEnvironment = EducationEnvironment.getInstance();
+import { LionCoreM3Environment } from "../../lionwebM3/config/gen/LionCoreM3Environment";
+export const editorEnvironment: FreEnvironment = LionCoreM3Environment.getInstance();
 LanguageInitializer.initialize();
 
 /**
