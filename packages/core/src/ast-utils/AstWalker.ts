12caf9ff9a213ccc04f640bb60fad3a10a65d3bc
Jos Warmer <jos.warmer@openmodeling.nl>
2023-10-09 20:51:37 +0200

    More rename element to node

--- packages/core/src/ast-utils/AstWalker.ts
+++ packages/core/src/ast-utils/AstWalker.ts
@@ -23,23 +23,23 @@ export class AstWalker {
      * none of the rest of the workers will be called. For that particular node both the 'execBefore' and 'execAfter'
      * method of the other workers will be skipped.
      *
-     * @param modelelement the top node of the part of the tree to be visited
+     * @param node the top node of the part of the tree to be visited
      * @param includeNode tests every child of 'modelelement', if true, it will also be visited
      */
-    public walk(modelelement: FreNode, includeNode?: (elem: FreNode) => boolean) {
+    public walk(node: FreNode, includeNode?: (elem: FreNode) => boolean) {
         if (this.myWorkers.length > 0) {
             let stopWalkingThisNode: boolean = false;
             for (const worker of this.myWorkers) {
                 if (!stopWalkingThisNode) {
-                    stopWalkingThisNode = worker.execBefore(modelelement);
+                    stopWalkingThisNode = worker.execBefore(node);
                 }
             }
 
             // find part properties in the language meta definition
-            const partProperties: FreLanguageProperty[] = FreLanguage.getInstance().getPropertiesOfKind(modelelement.freLanguageConcept(), "part");
+            const partProperties: FreLanguageProperty[] = FreLanguage.getInstance().getPropertiesOfKind(node.freLanguageConcept(), "part");
             // walk all parts
             for (const prop of partProperties) {
-                for (const child of FreLanguage.getInstance().getPropertyValue(modelelement, prop)) {
+                for (const child of FreLanguage.getInstance().getPropertyValue(node, prop)) {
                     if (includeNode === undefined || includeNode(child)) {
                         this.walk(child, includeNode);
                     }
@@ -48,7 +48,7 @@ export class AstWalker {
 
             for (const worker of this.myWorkers) {
                 if (!stopWalkingThisNode) {
-                    stopWalkingThisNode = worker.execAfter(modelelement);
+                    stopWalkingThisNode = worker.execAfter(node);
                 }
             }
         } else {
