31738f15208545c123624574c28153dccea390b1
mike.vogel <globallyunique@gmail.com>
2024-06-10 17:34:17 -0400

    showed that .ts class can be passed to .js

--- packages/server/src/__tests__/Simulator.ts
+++ packages/server/src/__tests__/Simulator.ts
@@ -1,6 +1,7 @@
 import { Schedule } from "./Schedule.js"
 import * as Sim from "../../../playground/src/StudyConfiguration/simjs/sim.js"
 import log from "./SimpleLogger.js";
+import { EventSchedule } from "./EventSchedule";
 
 
 export class Simulator {
@@ -8,13 +9,9 @@ export class Simulator {
 
   constructor() {
     this.sim = new Sim.Sim();
-    let visits = [
-      { name: 'Visit 1', day: 7, repetitions: 0, minWindow: 0, maxWindow: 0, dependency: null },
-      { name: 'Visit 2', day: 10, repetitions: 2, minWindow: 2, maxWindow: 4, dependency: null },
-      { name: 'Visit 3', day: null, repetitions: 0, minWindow: 2, maxWindow: 2, dependency: 'Visit 2' }
-      // Add more visits as needed
-    ];
-    Schedule.setVisits(visits);  
+    let eventSchedule = new EventSchedule();
+
+    Schedule.setVisits(eventSchedule.getVisits());  
     this.sim.addEntity(Schedule);
     this.sim.setLogger(function (str) {
        log(str);
