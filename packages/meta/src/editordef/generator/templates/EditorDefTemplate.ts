a9cfa8e8ff42f993e0396ef56c18c3a33c608a80
mike.vogel <globallyunique@gmail.com>
2024-07-01 17:56:24 -0400

    wip on making handler work

--- packages/meta/src/editordef/generator/templates/EditorDefTemplate.ts
+++ packages/meta/src/editordef/generator/templates/EditorDefTemplate.ts
@@ -184,6 +184,7 @@ export class EditorDefTemplate {
                     `${handlerVarName}.addProjection("${Names.projection(group)}")`
                 ).join(";\n")}
                 for (const p of freonConfiguration.customProjection) {
+                    p.handler = ${handlerVarName};
                     ${handlerVarName}.addCustomProjection(p);
                 }
                 handler.initConceptToPropertyProjection(map);
