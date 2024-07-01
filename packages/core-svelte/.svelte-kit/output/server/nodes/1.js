4a6656fa9f5f2022cf6b733be72aef4ddb89129b
Jos Warmer <jos.warmer@openmodeling.nl>
2024-07-01 11:44:37 +0200

    Remove .svelte-kit contents from git.

--- packages/core-svelte/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.rbO-lhAu.js","_app/immutable/chunks/scheduler.BgUxPr7R.js","_app/immutable/chunks/index.irzl9eKB.js","_app/immutable/chunks/entry.DoilO8n6.js"];
-export const stylesheets = [];
-export const fonts = [];
