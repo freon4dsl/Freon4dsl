8c387c1cf7a6841fdf02086a8128abf61e5f5dca
Jos Warmer <jos.warmer@openmodeling.nl>
2024-07-23 09:28:37 +0200

    Remove generated .sveltekit files from repo.

--- packages/core-svelte/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.BopIt6fK.js","_app/immutable/chunks/scheduler.BgUxPr7R.js","_app/immutable/chunks/index.irzl9eKB.js"];
-export const stylesheets = [];
-export const fonts = [];
