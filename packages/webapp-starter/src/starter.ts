eea4be8dcec7f3af8d83ea223bdd3a4b63aee6ff
Jos Warmer <jos.warmer@openmodeling.nl>
2024-12-20 16:10:03 +0100

    Make all saved models use new key

--- packages/webapp-starter/src/starter.ts
+++ packages/webapp-starter/src/starter.ts
@@ -1,21 +1,16 @@
-import { FreonLayout, WebappConfigurator } from "@freon4dsl/webapp-lib";
-import {FreLogger, ServerCommunication} from "@freon4dsl/core";
+import { FreonLayout, WebappConfigurator, setDevelopment } from "@freon4dsl/webapp-lib";
+import { ServerCommunication} from "@freon4dsl/core";
 // import { LionWebRepositoryCommunication } from "@freon4dsl/core"
-import { setDevelopment } from "@freon4dsl/webapp-lib";
 
 /**
  * The one and only reference to the actual language for which this editor runs
  */
-import {UndefinedEnvironment} from "@freon4dsl/samples-example";
-WebappConfigurator.getInstance().setEditorEnvironment(UndefinedEnvironment.getInstance());
-
-// import {LionCore_M3Environment} from "@freon4dsl/samples-lioncore";
-// WebappConfigurator.getInstance().setEditorEnvironment(LionCore_M3Environment.getInstance());
+import {ExampleEnvironment} from "@freon4dsl/samples-example";
+WebappConfigurator.getInstance().setEditorEnvironment(ExampleEnvironment.getInstance());
 
 /**
  * The one and only reference to the server on which the models are stored
  */
-// WebappConfigurator.getInstance().setServerCommunication(LionWebRepositoryCommunication.getInstance());
 WebappConfigurator.getInstance().setServerCommunication(ServerCommunication.getInstance());
 
 setDevelopment(true)
@@ -26,15 +21,15 @@ setDevelopment(true)
 const app = new FreonLayout({
     target: document.body,
 });
-FreLogger.unmute("FreLionwebSerializer")
-FreLogger.unmute("MobxDecorators")
+// FreLogger.unmute("FreLionwebSerializer")
+// FreLogger.unmute("MobxDecorators")
 // FreLogger.unmute("TextComponent")
 // FreLogger.unmute("TextDropdownComponent")
 // FreLogger.unmute("DropdownComponent")
 // FreLogger.unmute("EditorState")
 // FreLogger.unmute("InMemoryModel")
 // FreLogger.unmute("TextComponent")
-FreLogger.unmute("ServerCommunication")
-FreLogger.setFilter(["beforeUpdate", "setFocus", "afterUpdate", "onMount", "REFRESH", "refresh"])
+// FreLogger.unmute("ServerCommunication")
+// FreLogger.setFilter(["beforeUpdate", "setFocus", "afterUpdate", "onMount", "REFRESH", "refresh"])
 
 export default app;
