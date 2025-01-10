197ab19f498649963417f250913528951a6677ac
Jos Warmer <jos.warmer@openmodeling.nl>
2025-01-10 14:20:13 +0100

    Reorganize start app

---
+++ packages/webapp-starter/src/loggers.ts
@@ -0,0 +1,19 @@
+/**
+ * Configure loggers. Loggers are muted by default, unmuting loggers that you want to
+ * activate here.
+ */
+export function configureLoggers() {
+    // // Activate the following loggers
+    // FreLogger.unmute("FreLionwebSerializer")
+    // FreLogger.unmute("MobxDecorators")
+    // FreLogger.unmute("TextComponent")
+    // FreLogger.unmute("TextDropdownComponent")
+    // FreLogger.unmute("DropdownComponent")
+    // FreLogger.unmute("EditorState")
+    // FreLogger.unmute("InMemoryModel")
+    // FreLogger.unmute("TextComponent")
+    // FreLogger.unmute("ServerCommunication")
+    
+    // // Do not show log messages contaning any of these strings
+    // FreLogger.setFilter(["beforeUpdate", "setFocus", "afterUpdate", "onMount", "REFRESH", "refresh"])
+}
--- packages/webapp-starter/src/starter.ts
+++ packages/webapp-starter/src/starter.ts
@@ -1,19 +1,18 @@
 import { FreonLayout, WebappConfigurator, setDevelopment } from "@freon4dsl/webapp-lib";
+import { configureExternals } from "./externals.js";
+import { configureLoggers } from "./loggers.js";
+import { LanguageEnvironment } from "@freon4dsl/samples-example";
 import { ServerCommunication} from "@freon4dsl/core";
 // import { LionWebRepositoryCommunication } from "@freon4dsl/core"
 
 /**
- * The one and only reference to the actual language for which this editor runs
+ * Initialize everything
  */
-import { LanguageEnvironment } from "@freon4dsl/samples-example";
 WebappConfigurator.getInstance().setEditorEnvironment(LanguageEnvironment.getInstance());
-
-/**
- * The one and only reference to the server on which the models are stored
- */
 WebappConfigurator.getInstance().setServerCommunication(ServerCommunication.getInstance());
-
 setDevelopment(true)
+configureExternals()
+configureLoggers()
 
 /**
  * Now start the app ...
@@ -21,15 +20,4 @@ setDevelopment(true)
 const app = new FreonLayout({
     target: document.body,
 });
-// FreLogger.unmute("FreLionwebSerializer")
-// FreLogger.unmute("MobxDecorators")
-// FreLogger.unmute("TextComponent")
-// FreLogger.unmute("TextDropdownComponent")
-// FreLogger.unmute("DropdownComponent")
-// FreLogger.unmute("EditorState")
-// FreLogger.unmute("InMemoryModel")
-// FreLogger.unmute("TextComponent")
-// FreLogger.unmute("ServerCommunication")
-// FreLogger.setFilter(["beforeUpdate", "setFocus", "afterUpdate", "onMount", "REFRESH", "refresh"])
-
 export default app;
