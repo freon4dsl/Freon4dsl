83fabc77e2afa4b4249a6952606ebc9e307e2db1
Anneke Kleppe <anneke@warmerkleppe.nl>
2025-02-20 10:47:32 +0100

    Different approach to testing svelte components - removed test script from package.json

--- packages/webapp-lib/vite.config.ts
+++ packages/webapp-lib/vite.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from "vitest/config";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
-
-  test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"],
-  },
+  plugins: [sveltekit()]
 });
