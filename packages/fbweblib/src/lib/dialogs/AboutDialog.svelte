6645d59483580713478c3dbd5e04e58630d2fbcf
Anneke Kleppe <anneke@warmerkleppe.nl>
2025-03-05 15:52:31 +0100

    Cleaned up AboutDialog.svelte

--- packages/fbweblib/src/lib/dialogs/AboutDialog.svelte
+++ packages/fbweblib/src/lib/dialogs/AboutDialog.svelte
@@ -57,46 +57,49 @@
         <h3 class="mb-0 text-xl font-semibold pb-0 text-gray-900 dark:text-white">Hints for Using the Editor</h3>
 
         <div class="ml-4">
-        <h5 class="text-lg font-semibold text-gray-900 dark:text-white mb-2">Context menu</h5>
-<!-- todo check the hints, the enter key , for instance, does not work anymore -->
-        <p class="text-gray-700 dark:text-gray-300 mb-2">
-            Using the right mouse button a context menu can be shown for elements that are
-            part of a list. This menu offers some functionality on the selected list node, like cut/copy/paste.
-        </p>
+            <h5 class="text-lg font-semibold text-gray-900 dark:text-white mb-2">Context menu</h5>
+            <!-- todo check the hints, the enter key , for instance, does not work anymore -->
+            <p class="text-gray-700 dark:text-gray-300 mb-2">
+                Using the right mouse button a context menu can be shown for elements that are
+                part of a list. This menu offers some functionality on the selected list node, like cut/copy/paste.
+            </p>
 
-        <h5 class="text-lg font-semibold text-gray-900 dark:text-white mb-2">Drag and drop</h5>
-        <p class="text-gray-700 dark:text-gray-300 mb-6">
-            Drag and drop functionality is provided for list elements. An node can be dragged from a list (or table),
-            and dropped in a list (or table). Note that you should click outside of any editable text to grab the node.
-        </p>
+            <h5 class="text-lg font-semibold text-gray-900 dark:text-white mb-2">Drag and drop</h5>
+            <p class="text-gray-700 dark:text-gray-300 mb-6">
+                Drag and drop functionality is provided for list elements. An node can be dragged from a list (or
+                table), and dropped in a list (or table). Note that you should click outside of any editable text
+                to grab the node.
+            </p>
 
             <h5 class="text-lg font-semibold text-gray-900 dark:text-white mb-2">Keybindings</h5>
             <p class="text-gray-700 dark:text-gray-300 mb-6">
-            When editing a text field, e.g. a name, the ENTER key stops the editing and stores the value in the model,
-            whereas the ESC key stops the editing without storing the value.
-        </p>
+                When editing a text field, e.g. a name, the ENTER key stops the editing and stores the value in the
+                model, whereas the ESC key stops the editing without storing the value.
+            </p>
 
             <h5 class="text-lg font-semibold text-gray-900 dark:text-white mb-2">
-            Keys that can be used anywhere
-        </h5>
-        <ul class="list-disc">
-            <li class="ml-6 text-base border-none">
-                Arrow Keys move in the expected direction
-            </li>
-            <li class="ml-6 text-base border-none">
-                TAB key moves to next place where the user can type something
-            </li>
-
-            <li class="ml-6 text-base border-none">Ctrl-UP selects the parent node in the AST</li>
-
-            <li class="ml-6 text-base border-none">Ctrl-DOWN selects the first child node in the AST
-            </li>
-            <li class="ml-6 text-base border-none">Ctrl-SPACE shows popup menu of possible options
-            </li>
-            <li class="ml-6 text-base border-none">DEL deletes the next character or the currently selected
-                item from the AST
-            </li>
-        </ul>
+                Keys that can be used anywhere
+            </h5>
+            <ul class="list-disc">
+                <li class="ml-6 text-base border-none">
+                    Arrow Keys move in the expected direction
+                </li>
+                <li class="ml-6 text-base border-none">
+                    TAB key moves to next place where the user can type something
+                </li>
+                <li class="ml-6 text-base border-none">
+                    Ctrl-UP selects the parent node in the AST
+                </li>
+                <li class="ml-6 text-base border-none">
+                    Ctrl-DOWN selects the first child node in the AST
+                </li>
+                <li class="ml-6 text-base border-none">
+                    Ctrl-SPACE shows popup menu of possible options
+                </li>
+                <li class="ml-6 text-base border-none">
+                    DEL deletes the next character or the currently selected item from the AST
+                </li>
+            </ul>
         </div>
     </Card>
 </Modal>
