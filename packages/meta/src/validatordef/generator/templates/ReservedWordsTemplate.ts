7f044817eed1d7965832297b3bde33c0b027dcfb
annekekleppe <anneke@warmerkleppe.nl>
2025-04-02 16:31:40 +0200

    Added a copy of ReservedWords.ts to validatordef/generator/templates to avoid the error 'ENOENT: no such file or directory, open 'C:\Users\aklep\WebstormProjects\Freon4dsl\packages\meta\dist\validatordef\generator\templates\ReservedWords.js'', which still needs to be fixed.

--- packages/meta/src/validatordef/generator/templates/ReservedWordsTemplate.ts
+++ packages/meta/src/validatordef/generator/templates/ReservedWordsTemplate.ts
@@ -1,9 +1,11 @@
 import * as fs from "fs";
+import path from 'node:path';
 
-const constFile = import.meta.dirname + "/ReservedWords.ts";
+const constFile = import.meta.dirname + path.sep + "ReservedWords.js";
 
 export class ReservedWordsTemplate {
     generateConst(): string {
+        console.log("Generating ReservedWords...", constFile);
         return fs.readFileSync(constFile, { encoding: "utf8" });
     }
 }
