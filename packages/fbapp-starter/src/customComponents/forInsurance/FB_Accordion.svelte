c1be73a46a6a19aefa6941826dfb8189069a7c6e
annekekleppe <anneke@warmerkleppe.nl>
2025-04-03 11:22:04 +0200

    Improved component based on Flowbite Accordion

--- packages/fbapp-starter/src/customComponents/forInsurance/FB_Accordion.svelte
+++ packages/fbapp-starter/src/customComponents/forInsurance/FB_Accordion.svelte
@@ -10,17 +10,25 @@
     let panelOpen: boolean[] = $state([]);
     let multipleStr: string | undefined = box.findParam("multi");
     let multiplePar: boolean = $state(!!multipleStr && multipleStr.length > 0);
-    let key: string = "name";
+    let headerContent: string[] = $state([]);
     // <html>Svelte: Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'FreNode'.<br/>No index signature with a parameter of type 'string' was found on type 'FreNode'.
 
     function initialize() {
         let multipleStr: string | undefined = box.findParam("multi");
         multiplePar = !!multipleStr && multipleStr.length > 0;
+        let tmpHeaderContent: string[] = []; // to avoid triggering the effect multiple times.
         for (let i = 0; i < box.children.length; i++) {
             // this also sets the length of panelOpen!
             panelOpen[i] = false;
             box.children[i].isVisible = false; // the child boxes are not currently shown
+            // We know that node is of type FreNamedNode, because this component is only used to
+            // show 'parts: InsurancePart[]', so we ignore the compiler error.
+            // This is also the reason that we determine the header content here.
+            // Within the html part of this component we cannot use ts-ignore.
+            // @ts-ignore
+            tmpHeaderContent.push(box.children[i].node["name"]);
         }
+        headerContent = tmpHeaderContent;
     }
 
     function setHidden(index: number) {
@@ -48,22 +56,21 @@
 
     // execute initialize()
     initialize()
-    // todo restore {childBox.node["name"]} on Header
 
-    // <html>Svelte: Element implicitly has an 'any' type because expression of type '&quot;icon&quot;' can't be used to index type '{}'.<br/>Property 'icon' does not exist on type '{}'.
 </script>
 
 <Accordion multiple={multiplePar}>
     {#each box.children as childBox, index}
-        <AccordionItem bind:open={panelOpen[index]}>
-            <span slot="header">
-                {childBox.node.freLanguageConcept()}
-<!--                <IconButton toggle pressed={panelOpen[index]} onclick={() => setHidden(index)}>-->
-<!--                    <Icon class="material-icons" on>expand_less</Icon>-->
-<!--                    <Icon class="material-icons">expand_more</Icon>-->
-<!--                </IconButton>-->
+        <!-- the following div is here only to catch the opening/closing of the panel -->
+        <!-- it must be in this position to catch the click, but it affects the styling -->
+        <!--  todo improve styling: rounded top corners on first element only -->
+        <div onclick={() => setHidden(index)} role="presentation">
+        <AccordionItem bind:open={panelOpen[index]} >
+            <span slot="header" class="w-[85vw] text-black dark:text-white" >
+                {childBox.node.freLanguageConcept()} <span class="text-primary-700 dark:text-primary-100">{headerContent[index]}</span>
             </span>
             <RenderComponent box={childBox} editor={editor} />
         </AccordionItem>
+        </div>
     {/each}
 </Accordion>
