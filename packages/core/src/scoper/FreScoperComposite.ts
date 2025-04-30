fb385c90acf83bad2cbf48739dce7c2fa4243084
annekekleppe <anneke@warmerkleppe.nl>
2025-04-30 08:50:57 +0200

    Added test on node param being null/undefined in all methods

--- packages/core/src/scoper/FreScoperComposite.ts
+++ packages/core/src/scoper/FreScoperComposite.ts
@@ -5,7 +5,6 @@ import { FreScoper } from "./FreScoper.js";
 const LOGGER = new FreLogger("FreScoperComposite").mute();
 
 export class FreScoperComposite implements FreScoper {
-    // TOD: What of this?
     mainScoper: FreScoperComposite;
     private scopers: FreScoper[] = [];
     name: string = "";
@@ -25,61 +24,73 @@ export class FreScoperComposite implements FreScoper {
     }
 
     additionalNamespaces(node: FreNode): FreNode[] {
-        for (const scoper of this.scopers) {
-            const result = scoper.additionalNamespaces(node);
-            if (result !== undefined && result !== null) {
-                return result;
+        if (!!node) {
+            for (const scoper of this.scopers) {
+                const result = scoper.additionalNamespaces(node);
+                if (result !== undefined && result !== null) {
+                    return result;
+                }
             }
         }
         return [];
     }
 
     getFromVisibleElements(node: FreNode, name: string, metatype?: string, excludeSurrounding?: boolean): FreNamedNode {
-        for (const scoper of this.scopers) {
-            const result = scoper.getFromVisibleElements(node, name, metatype, excludeSurrounding);
-            if (result !== undefined) {
-                return result;
+        if (!!node) {
+            for (const scoper of this.scopers) {
+                const result = scoper.getFromVisibleElements(node, name, metatype, excludeSurrounding);
+                if (result !== undefined) {
+                    return result;
+                }
             }
         }
         return null;
     }
 
     getVisibleElements(node: FreNode, metatype?: string, excludeSurrounding?: boolean): FreNamedNode[] {
-        for (const scoper of this.scopers) {
-            const result = scoper.getVisibleElements(node, metatype, excludeSurrounding);
-            if (result !== undefined && result !== null) {
-                return result;
+        if (!!node) {
+            for (const scoper of this.scopers) {
+                const result = scoper.getVisibleElements(node, metatype, excludeSurrounding);
+                if (result !== undefined && result !== null) {
+                    return result;
+                }
             }
         }
         return [];
     }
 
     getVisibleNames(node: FreNode, metatype?: string, excludeSurrounding?: boolean): string[] {
-        LOGGER.log("getVisibleNames for " + node.freLanguageConcept() + " of type " + metatype);
-        for (const scoper of this.scopers) {
-            const result = scoper.getVisibleNames(node, metatype, excludeSurrounding);
-            if (result !== undefined && result !== null) {
-                return result;
+        LOGGER.log("getVisibleNames for " + node?.freLanguageConcept() + " of type " + metatype);
+        if (!!node) {
+            for (const scoper of this.scopers) {
+                const result = scoper.getVisibleNames(node, metatype, excludeSurrounding);
+                if (result !== undefined && result !== null) {
+                    return result;
+                }
             }
         }
         return [];
     }
 
     isInScope(node: FreNode, name: string, metatype?: string, excludeSurrounding?: boolean): boolean {
-        for (const scoper of this.scopers) {
-            const result = scoper.isInScope(node, name, metatype, excludeSurrounding);
-            if (result !== undefined) {
-                return result;
+        if (!!node) {
+            for (const scoper of this.scopers) {
+                const result = scoper.isInScope(node, name, metatype, excludeSurrounding);
+                if (result !== undefined) {
+                    return result;
+                }
             }
         }
         return false; // TODO or undefined?
     }
 
     resolvePathName(node: FreNode, doNotSearch: string, pathname: string[], metatype?: string): FreNamedNode {
-        for (const scoper of this.scopers) {
-            const result = scoper.resolvePathName(node, doNotSearch, pathname, metatype);
-            if (result !== undefined) {
-                return result;
+        if (!!node) {
+            for (const scoper of this.scopers) {
+                const result = scoper.resolvePathName(node, doNotSearch, pathname, metatype);
+                if (result !== undefined) {
+                    return result;
+                }
             }
         }
         return null; // TODO or undefined?
