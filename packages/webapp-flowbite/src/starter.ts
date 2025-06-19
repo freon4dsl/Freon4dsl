647936e5ac05d7085672c65b14f36b4a069041fe
annekekleppe <anneke@warmerkleppe.nl>
2025-06-19 10:31:46 +0200

    Renamed of style variables in core-svelte, adjusted webapps, and weblibs

--- packages/webapp-flowbite/src/starter.ts
+++ packages/webapp-flowbite/src/starter.ts
@@ -1,5 +1,5 @@
 import { mount } from 'svelte'
-import { FlowbiteFreonLayout, WebappConfigurator, setDevelopment } from "@freon4dsl/flowbite-webapp"
+import { FlowbiteFreonLayout, WebappConfigurator, setDevelopment } from "@freon4dsl/weblib-flowbite"
 import { configureExternals } from "./externals.js"
 import { configureLoggers } from "./loggers.js"
 import { LanguageEnvironment } from "@freon4dsl/samples-education"
