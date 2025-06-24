0ad2a5d438fff3b27a25ecf8f2ae5a502e4f6f55
annekekleppe <anneke@warmerkleppe.nl>
2025-06-24 09:46:09 +0200

    Working on developer documentation

--- packages/fbapp-starter/src/starter.ts
+++ packages/fbapp-starter/src/starter.ts
@@ -2,7 +2,7 @@ import { mount } from 'svelte'
 import { FlowbiteFreonLayout, WebappConfigurator, setDevelopment } from "@freon4dsl/flowbite-webapp"
 import { configureExternals } from "./externals.js"
 import { configureLoggers } from "./loggers.js"
-import { LanguageEnvironment } from "@freon4dsl/samples-external-tester"
+import { LanguageEnvironment } from "@freon4dsl/samples-scoper-test"
 import { ServerCommunication } from "@freon4dsl/core"
 
 /**
