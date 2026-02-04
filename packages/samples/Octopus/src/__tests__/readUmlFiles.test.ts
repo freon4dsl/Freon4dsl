027a60cd5236dec7e3ece72b9dc15347e4f7bc99
Jos Warmer <jos.warmer@openmodeling.nl>
2026-02-04 11:43:51 +0100

    Put AST changing code inside FREON.astChanger.change()

--- packages/samples/Octopus/src/__tests__/readUmlFiles.test.ts
+++ packages/samples/Octopus/src/__tests__/readUmlFiles.test.ts
@@ -1,6 +1,6 @@
 import {OctopusModel, UmlPart} from "../freon/language/index.js";
 import { OctopusModelEnvironment } from "../freon/config/OctopusModelEnvironment.js";
-import { CoreConfig, FreModelSerializer } from "@freon4dsl/core"
+import { CoreConfig, FreModelSerializer, FREON } from "@freon4dsl/core"
 import {describe, test, expect} from "vitest";
 import { FileUtil } from "@freon4dsl/test-helpers"
 
@@ -13,12 +13,16 @@ function compareReadAndWrittenUmlParts(filepath: string) {
         const model: OctopusModel = new OctopusModel();
         const langSpec: string = FileUtil.stringFromFile('./src/__inputs__/' + filepath);
         const unit1 = reader.readFromString(langSpec, "UmlPart", model) as UmlPart;
-        unit1.name = "unit1";
+        FREON.astChanger.change( () => {
+            unit1.name = "unit1";
+        })
         let result: string = writer.writeToString(unit1, 0, false);
         expect(result.length).toBeGreaterThan(0);
         // handler.stringToFile(filepath + "2", result);
         const unit2 = reader.readFromString(result, "UmlPart", model) as UmlPart;
-        unit2.name = "unit1"; // the names should be the same in the comparison, but may not be the same in the reader method!!!
+        FREON.astChanger.change( () => {
+            unit2.name = "unit1"; // the names should be the same in the comparison, but may not be the same in the reader method!!!
+        })
         // simply comparing the units does not work because the id properties of the two units
         // are not the same, therefore we use the hack of checking whether both units in JSON
         // format are the same
