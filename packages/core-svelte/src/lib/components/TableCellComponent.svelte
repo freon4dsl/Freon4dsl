1dbed5edf15511ad57649456565c06a8d8395f2d
Jos Warmer <jos.warmer@openmodeling.nl>
2026-03-13 17:19:46 +0100

    Typos

--- packages/core-svelte/src/lib/components/TableCellComponent.svelte
+++ packages/core-svelte/src/lib/components/TableCellComponent.svelte
@@ -149,7 +149,6 @@
             FREON.astChanger.changeNamed("ListComponent.Enter", () => {
                 execresult = action.execute(tableBox, { meta: MetaKey.None, key: ENTER, code: ENTER }, editor, selectedIndex)
             })
-            // @ts-ignore
             if (notNullOrUndefined(execresult)) {
                 execresult();
             }
@@ -186,7 +185,7 @@
 
     const dragenter = (event: DragEvent): boolean => {
         let data: ListElementInfo | null = draggedElem.value;
-        if (notNullOrUndefined(data) {
+        if (notNullOrUndefined(data)) {
             if (isFreNodeReference(data.element)) {
                 LOGGER.log(`dragEnter item [${data.element.name}] from [${data.componentId}] in table [${id}] on position [${row},${column}]`);
             } else if (isFreNode(data.element)) {
@@ -234,7 +233,7 @@
             let index: number;
             // determine the contents of the menu based on box
             // if the selected box is the placeholder or a title/header => show different menu items
-            let items: MenuItem[] = [];
+            let items: MenuItem[];
             if (isActionBox(box.content)) {
                 items = box.options(MenuOptionsType.placeholder);
                 index = Number.MAX_VALUE;
